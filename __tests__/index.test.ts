e3b4a5bfd4eb1d7a4324fc049abe375e1f70f859
Joe Previte <jjprevite@gmail.com>
2022-03-10 08:28:25 -0700

    initial commit

---
+++ __tests__/index.test.ts
@@ -0,0 +1,21 @@
+import { JSDOM } from "jsdom";
+import { streakCounter } from "../src/index";
+
+describe("streakCounter", () => {
+  let mockLocalStorage: Storage;
+
+  beforeEach(() => {
+    const mockJSDom = new JSDOM("", { url: "https://localhost" });
+
+    mockLocalStorage = mockJSDom.window.localStorage;
+  });
+
+  it("should return a streak object with currentCount, startDate and lastLoginDate", () => {
+    const date = new Date();
+    const streak = streakCounter(mockLocalStorage, date);
+
+    expect(streak.hasOwnProperty("currentCount")).toBe(true);
+    expect(streak.hasOwnProperty("startDate")).toBe(true);
+    expect(streak.hasOwnProperty("lastLoginDate")).toBe(true);
+  });
+});
